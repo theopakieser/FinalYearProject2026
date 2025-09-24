482af881855a2a6ca6962e87432981e2565f87b4
theopakieser <20101918@mail.wit.ie>
2025-09-24 12:32:34 +0100

    scan_folder method that lists the files in a given folder, CLI based

--- src/fic/scanner.py
+++ src/fic/scanner.py
@@ -1,2 +1,15 @@
-#Code to go through directories and hash files
+
+# File: scanner.py
+# Description: Code to go through directories and hash files
+# Author: Theo Pakieser
+# Date: 24/9/2025
+
+#Imports
+from .utils import calculate_hash
+import os
+
+def scan_folder(folder_path: str):
+    #Return list of files in a given folder
+    return [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
 
